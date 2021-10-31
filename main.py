0d08ac8f0d6364cf99d1d178e15c37b08d38a7a0
takaaki-miyake <takaaki.miyake@seattleconsulting.co.jp>
2021-10-31 23:15:30 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+import requests
+# Use the package we installed
+from slack_bolt import App
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+# Add functionality here
+# @app.event("app_home_opened") etc
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+      # the user that opened your app's app home
+      user_id=event["user"],
+      # the view object that appears in the app home
+      view={
+        "type": "home",
+        "callback_id": "home_view",
+
+        # body of the view
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    )
+
+  except Exception as e:
+    logger.error(f"Error opening modal: {e}")
+
+@app.event("app_mention")
+def getmsg():
+    CHANNEL = 'CHN7K9TA8'
+    url = "https://slack.com/api/conversations.history"
+    headers = {"Authorization": "Bearer "+os.environ.get("SLACK_BOT_TOKEN")}
+    params = {
+        "channel": CHANNEL,
+        "limit": 10
+    }
+    r = requests.get(url, headers=headers, params=params)
+    print("return ", r.json())
+
+# Start your app
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
